6bdcb1d5439a32e6373c08ec814c28acc25452c8
Edgar Mamani Gutierrez <edgel.infor@gmail.com>
2021-08-12 10:54:57 -0400

    Ajustes para desplegar web

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const urlApiProducts = "https://my-json-server.typicode.com/darkedg/shopping-cart-react/producs"; // https://my-json-server.typicode.com/
-export const BASE_PATH = "https://darkedg.github.io/shopping-cart-react/";
+export const BASE_PATH = "https://darkedg.github.io/";
 export const STORAGE_PRODUCTS_CART = "productsCart";
